7889cb7483b02cdd314d73377f97bae845439345
Fabricevandeurzenpersonal <Fabricevandeurzen2@gmail.com>
2026-01-22 14:25:52 +0100

    dag 4
    de program.cs opschonen en  menu.cs en game.cs gaan gebruiken

---
+++ oefenenConsoleC#/Game.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection.Metadata;
+using Tamagotchi.models;
+
+namespace Tamagotchi;
+
+public class Game
+{
+    private Menu _menu = new Menu();
+    private List<DigitalPet> _pets = new List<DigitalPet>();
+
+    public void Start()
+    {
+        _menu.ShowTitle();
+
+        bool appRunning = true;
+
+        while (appRunning)
+        {
+            _menu.ShowMainMenu(_pets.Count);
+
+            string choice = _menu.AskString("Keuze:");
+            if (choice.ToUpper() == "Q")
+            {
+                appRunning = false;
+            }
+        }
+    }
+}
---
+++ oefenenConsoleC#/Menu.cs
@@ -0,0 +1,33 @@
+﻿using Figgle.Fonts;
+namespace Tamagotchi;
+
+public class Menu
+{
+    public void ShowTitle()
+    {
+        Console.Clear();
+        try { Console.WriteLine(FiggleFonts.Standard.Render("Tamagotchi")); }
+        catch { Console.WriteLine("===Tamagotchi==="); }
+    }
+
+    public string AskString(string question)
+    {
+        Console.Write(question + " ");
+        return Console.ReadLine() ?? " ";
+    }
+    
+    public void WaitForEnter()
+    {
+        Console.WriteLine("\nDruk op Enter om verder te gaan...");
+        Console.ReadLine();
+    }
+    public void ShowMainMenu(int petCount)
+    {
+        Console.WriteLine($"\n--- HOOFDMENU ({petCount} dieren) ---");
+        Console.WriteLine("1. Voeg een nieuw huisdier toe");
+        Console.WriteLine("2. Bekijk je huisdieren");
+        Console.WriteLine("3. Dier zoeken & Selecteren");
+        Console.WriteLine("Q. Stoppen");
+        Console.Write("Maak een keuze:");
+    }
+}
